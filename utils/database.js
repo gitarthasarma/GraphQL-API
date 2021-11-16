da40a944512b3a75d7be462e492c442be062f5ca
Gitartha Kumar Sarma <61044554+gitarthasarma@users.noreply.github.com>
2021-11-16 20:02:24 +0530

    updated

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,8 @@
 const Sequelize = require('sequelize');
 
-// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     dialect: 'mysql',
-//     host: process.env.DB_HOST
-// });
-
-
-const db = new Sequelize('upbringo', 'root', 'password', {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
 });
 
 module.exports = db;
